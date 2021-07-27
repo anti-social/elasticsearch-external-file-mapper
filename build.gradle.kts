c08e8fd853f123fbc0ae3acd33eb6d1780f91bb3
Alexander Koval <kovalidis@gmail.com>
2021-07-27 18:58:45 +0300

    Debian package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-import org.elasticsearch.gradle.VersionProperties
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.nio.file.Paths
 
 buildscript {
     // val defaultEsVersion = "6.8.12"
@@ -20,6 +19,7 @@ plugins {
     java
     kotlin("jvm") version "1.3.50"
     id("org.ajoberstar.grgit") version "3.1.1"
+    id("nebula.ospackage") version "8.5.6"
 }
 
 apply {
@@ -32,8 +32,10 @@ subprojects {
     }
 }
 
+val pluginName = "mapper-external-file"
+
 configure<org.elasticsearch.gradle.plugin.PluginPropertiesExtension> {
-    name = "mapper-external-file"
+    name = pluginName
     description = "External file field mapper for ElasticSearch"
     classname = "company.evo.elasticsearch.plugin.mapper.ExternalFileMapperPlugin"
     licenseFile = rootProject.file("LICENSE.txt")
@@ -43,7 +45,9 @@ configure<org.elasticsearch.gradle.plugin.PluginPropertiesExtension> {
 val grgit: org.ajoberstar.grgit.Grgit by extra
 val tag = grgit.describe(mapOf("tags" to true, "match" to listOf("v*"))) ?: "v0.0.0"
 version = tag.trimStart('v')
-val versions = VersionProperties.getVersions() as Map<String, String>
+val esVersions = org.elasticsearch.gradle.VersionProperties.getVersions() as Map<String, String>
+
+val distDir = Paths.get(buildDir.path, "distributions")
 
 repositories {
     mavenCentral()
@@ -52,10 +56,10 @@ repositories {
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile("company.evo:persistent-hashmap")
-    compile("commons-logging", "commons-logging", versions["commonslogging"])
+    compile("commons-logging", "commons-logging", esVersions["commonslogging"])
 }
 
-tasks.withType(KotlinCompile::class.java).all {
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
@@ -73,5 +77,29 @@ tasks.register("listRepos") {
                 }
                 println("Name: ${repo.name}; url: $repoUrl")
             }
+    }
+}
+
+tasks.named("assemble") {
+    dependsOn("deb")
+}
+
+tasks.register("deb", com.netflix.gradle.plugins.deb.Deb::class) {
+    dependsOn("bundlePlugin")
+
+    packageName = "elasticsearch-${pluginName}-plugin"
+    requires("elasticsearch", esVersions["elasticsearch"])
+        .or("elasticsearch-oss", esVersions["elasticsearch"])
+
+    from(zipTree(tasks["bundlePlugin"].outputs.files.singleFile))
+
+    val esHome = project.properties["esHome"] ?: "/usr/share/elasticsearch"
+    into("$esHome/plugins/${pluginName}")
+
+    doLast {
+        if (properties.containsKey("assembledInfo")) {
+            distDir.resolve("assembled-deb.filename").toFile()
+                .writeText(assembleArchiveName())
+        }
     }
 }
